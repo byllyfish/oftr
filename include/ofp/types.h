0f574c7334b220c92e8d29fe64d668f23364ec26
Bill Fisher <william.w.fisher@gmail.com>
2013-08-03 20:06:08 -0700

    Added comments. Reformat using clang-format.

--- include/ofp/types.h
+++ include/ofp/types.h
@@ -4,135 +4,178 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// Include common headers. Define useful types, functions and macros. 
+// Include common headers. Define useful types, functions and macros.
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef OFP_TYPES
-#define OFP_TYPES
+#ifndef OFP_TYPES_H
+#define OFP_TYPES_H
 
-#include <cstddef>				// for std::size_t, etc.
-#include <cstdint>				// for std::uint8_t, etc.
-#include <cstdlib>				// for std::memcpy, etc.
-#include <type_traits>			// for std::make_unsigned<T>, etc.
+#include <cstddef>     // for std::size_t, etc.
+#include <cstdint>     // for std::uint8_t, etc.
+#include <cstdlib>     // for std::memcpy, etc.
+#include <type_traits> // for std::make_unsigned<T>, etc.
 #include <string>
 
 namespace ofp { // <namespace ofp>
 
 // Metaprogramming Utilities
 
-template<bool B, typename T>
-using EnableIf = typename std::enable_if<B,T>::type;
+template <bool B, typename T>
+using EnableIf = typename std::enable_if<B, T>::type;
 
-template<bool B, typename T, typename F>
-using Conditional = typename std::conditional<B,T,F>::type;
+template <bool B, typename T, typename F>
+using Conditional = typename std::conditional<B, T, F>::type;
 
-template<typename T>
-using MakeUnsigned = typename std::make_unsigned<T>::type;
-
+template <typename T> using MakeUnsigned = typename std::make_unsigned<T>::type;
 
 // Be careful when using UInt8 and UInt16. By default, C++ will promote these
 // to an `int` for bitwise and arithmetic operations, so you need to be
 // alert for sign extension. Prefer operations against `unsigned int` types.
 // See 10.5.1 in TC++PL, 4th ed.
- 
-using UInt8  = std::uint8_t;
+
+using UInt8 = std::uint8_t;
 using UInt16 = std::uint16_t;
 using UInt32 = std::uint32_t;
 using UInt64 = std::uint64_t;
 
-template <class T>
-inline constexpr
-MakeUnsigned<T> Unsigned_cast(T value) {
-	return static_cast<MakeUnsigned<T>>(value);
+template <class T> inline constexpr MakeUnsigned<T> Unsigned_cast(T value)
+{
+    return static_cast<MakeUnsigned<T>>(value);
 }
 
-template <class T>
-inline constexpr 
-UInt8 UInt8_cast(T value) {
-	static_assert(sizeof(T) == sizeof(UInt8), "Use UInt8_narrow_cast().");
-	return static_cast<UInt8>(Unsigned_cast(value));
+template <class T> inline constexpr UInt8 UInt8_cast(T value)
+{
+    static_assert(sizeof(T) == sizeof(UInt8), "Use UInt8_narrow_cast().");
+    return static_cast<UInt8>(Unsigned_cast(value));
 }
 
-template <class T>
-inline constexpr 
-UInt16 UInt16_cast(T value) {
-	static_assert(sizeof(T) <= sizeof(UInt16), "Use UInt16_narrow_cast().");
-	return static_cast<UInt16>(Unsigned_cast(value));
+template <class T> inline constexpr UInt16 UInt16_cast(T value)
+{
+    static_assert(sizeof(T) <= sizeof(UInt16), "Use UInt16_narrow_cast().");
+    return static_cast<UInt16>(Unsigned_cast(value));
 }
 
-template <class T>
-inline constexpr 
-UInt32 UInt32_cast(T value) {
-	static_assert(sizeof(T) <= sizeof(UInt32), "Use UInt32_narrow_cast().");
-	return static_cast<UInt32>(Unsigned_cast(value));
+template <class T> inline constexpr UInt32 UInt32_cast(T value)
+{
+    static_assert(sizeof(T) <= sizeof(UInt32), "Use UInt32_narrow_cast().");
+    return static_cast<UInt32>(Unsigned_cast(value));
 }
 
-template <class T>
-inline constexpr
-UInt64 UInt64_cast(T value) {
-	static_assert(sizeof(T) <= sizeof(UInt64), "No UInt64 narrow casts.");
-	return static_cast<UInt64>(Unsigned_cast(value));
+template <class T> inline constexpr UInt64 UInt64_cast(T value)
+{
+    static_assert(sizeof(T) <= sizeof(UInt64), "No UInt64 narrow casts.");
+    return static_cast<UInt64>(Unsigned_cast(value));
 }
 
-template <class T>
-inline constexpr 
-UInt8 UInt8_narrow_cast(T value) {
-	static_assert(sizeof(T) != sizeof(UInt8), "Use UInt8_cast().");
-	return static_cast<UInt8>(value);
+template <class T> inline constexpr UInt8 UInt8_narrow_cast(T value)
+{
+    static_assert(sizeof(T) != sizeof(UInt8), "Use UInt8_cast().");
+    return static_cast<UInt8>(value);
 }
 
-template <class T>
-inline constexpr 
-UInt16 UInt16_narrow_cast(T value) {
-	static_assert(sizeof(T) != sizeof(UInt16), "Use UInt16_cast().");
-	return static_cast<UInt16>(value);
+template <class T> inline constexpr UInt16 UInt16_narrow_cast(T value)
+{
+    static_assert(sizeof(T) != sizeof(UInt16), "Use UInt16_cast().");
+    return static_cast<UInt16>(value);
 }
 
-template <class T>
-inline constexpr 
-UInt16 UInt32_narrow_cast(T value) {
-	static_assert(sizeof(T) != sizeof(UInt32), "Use UInt32_cast().");
-	return static_cast<UInt32>(value);
+template <class T> inline constexpr UInt16 UInt32_narrow_cast(T value)
+{
+    static_assert(sizeof(T) != sizeof(UInt32), "Use UInt32_cast().");
+    return static_cast<UInt32>(value);
 }
 
 // Use Unsigned_difference() to subtract unsigned integers.
-template <class T>
-inline
-T Unsigned_difference(T lhs, T rhs) {
-	return lhs >= rhs ? lhs - rhs : 0;
+template <class T> inline T Unsigned_difference(T lhs, T rhs)
+{
+    return lhs >= rhs ? lhs - rhs : 0;
 }
 
 // Convenience function to check for literal type.
-template<typename T>
-constexpr bool IsLiteralType() { return std::is_literal_type<T>::value; }
-
-// Convenience function to check for standard layout.
-template<typename T>
-constexpr bool IsStandardLayout() { return std::is_standard_layout<T>::value; }
-
-// Return the number of items in an array.
-template<class T, size_t N>
-inline constexpr 
-size_t ArrayLength(T (&)[N]) 
+template <class T> constexpr bool IsLiteralType()
 {
-	return N;
+    return std::is_literal_type<T>::value;
 }
 
-// Convert a raw struct/buffer to a hexadecimal string.
-std::string RawDataToHex(const void *data, size_t len);
-std::string RawDataToHex(const void *data, size_t len, char delimiter, int word = 1);
+/**
+ * Convenience function to check for standard layout.
+ * @return true if type's layout is compatible with C.
+ */
+template <class T> constexpr bool IsStandardLayout()
+{
+    return std::is_standard_layout<T>::value;
+}
 
-// Convert a hexadecimal string to a raw struct/buffer. Only write up to maxlen
-// bytes. Return the number of bytes written. Set remaining bytes, if any, to 
-// zero. Ignore non-hex digits. If the number of hex digits is odd, ignore the 
-// last hex digit.
-size_t HexToRawData(const std::string &hex, void *data, size_t maxlen);
+/**
+ * Return the number of elements in an array.
+ *
+ * @return number of elements in an array.
+ */
+template <class T, size_t N> constexpr size_t ArrayLength(T(&)[N])
+{
+    return N;
+}
+
+/**
+ * Convert raw buffer to a hexadecimal string. The resulting string contains
+ * only hexadecimal characters.
+ *
+ * @param  data pointer to input buffer
+ * @param  length size of input buffer
+ * @return hexadecimal string
+ */
+std::string RawDataToHex(const void *data, size_t len);
+
+/**
+ * Convert raw buffer to a hexadecimal string. The resulting string is formatted
+ * according to `delimiter` and `word`. The delimiter specifies a character to
+ * insert between each run of hexadecimal chars. `word` specifies the number of
+ * bytes between delimiters.
+ *
+ * For example, delimiter=':' and word=2 produces "0001:0203:0405"
+ *
+ * @param  data pointer to raw memory
+ * @param  length size of input buffer
+ * @param  delimiter character to insert to format the hex string
+ * @param  word number of bytes between delimiters
+ * @return hexadecimal string
+ */
+std::string RawDataToHex(const void *data, size_t length, char delimiter,
+                         int word = 1);
+
+/**
+ * Convert a hexadecimal string to raw memory. Only write up to `length` bytes.
+ * Ignore non-hex digits and the odd final hex digit. If there are fewer than
+ * `length` bytes converted from the hex string, set the remaining bytes to
+ * zero.
+ *
+ * @param  hex string containing hexadecimal characters
+ * @param  data output buffer
+ * @param  length size of output buffer
+ * @return number of bytes resulting from hexadecimal string
+ */
+size_t HexToRawData(const std::string &hex, void *data, size_t length);
+
+/**
+ * Convert a hexadecimal string to raw memory. Ignore non-hex digits and the odd
+ * final hex digit.
+ *
+ * @param  hex string containing hexadecimal characters
+ * @return string containing raw bytes
+ */
 std::string HexToRawData(const std::string &hex);
 
-// Return true if every byte in block is set to char.
-bool MemFilled(const void *data, size_t len, char ch);
+/**
+ * Return true if memory block is filled with given byte value.
+ *
+ * @param  data pointer to memory block
+ * @param  length length of memory block
+ * @param  ch fill byte
+ * @return true if memory block is filled with given byte value.
+ */
+bool MemFilled(const void *data, size_t length, char fill);
 
 } // </namespace ofp>
 
-#endif // OFP_TYPES
+#endif // OFP_TYPES_H
