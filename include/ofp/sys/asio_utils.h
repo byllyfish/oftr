5f251198b585d8a0ca77ce0bb56c16dbc5b22039
Bill Fisher <william.w.fisher@gmail.com>
2014-03-17 16:58:44 -0700

    Reformat code.

--- include/ofp/sys/asio_utils.h
+++ include/ofp/sys/asio_utils.h
@@ -13,10 +13,10 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-//  
+//
 //  ===== ------------------------------------------------------------ =====  //
 /// \file
-/// \brief Common header file for Boost ASIO library that defines some support 
+/// \brief Common header file for Boost ASIO library that defines some support
 /// functions.
 ///
 /// Specify all ASIO includes here.
@@ -41,44 +41,42 @@ using udp = asio::ip::udp;
 using PlaintextSocket = Plaintext<tcp::socket>;
 using EncryptedSocket = asio::ssl::stream<tcp::socket>;
 
+/// Convert an asio::ip::address into an ofp::IPv6Address.
+inline IPv6Address makeIPv6Address(const asio::ip::address &addr) {
+  if (addr.is_v6()) {
+    return IPv6Address{addr.to_v6().to_bytes()};
+  } else {
+    assert(addr.is_v4());
+    IPv4Address v4{addr.to_v4().to_bytes()};
+    return IPv6Address{v4};
+  }
+}
 
-inline IPv6Address makeIPv6Address(const asio::ip::address &addr)
-{
-    if (addr.is_v6()) {
-        return IPv6Address{addr.to_v6().to_bytes()};
-    } else {
-        assert(addr.is_v4());
-        IPv4Address v4{addr.to_v4().to_bytes()};
-        return IPv6Address{v4};
-    }
+/// Create an asio::<proto>::endpoint from an ofp::IPv6Address and port number.
+template <class Proto>
+inline typename Proto::endpoint makeEndpoint(const IPv6Address &addr,
+                                             UInt16 port) {
+  using Endpoint = typename Proto::endpoint;
+
+  if (!addr.valid()) {
+    return Endpoint{Proto::v6(), port};
+  } else if (addr.isV4Mapped()) {
+    asio::ip::address_v4 v4{addr.toV4().toArray()};
+    return Endpoint{v4, port};
+  } else {
+    asio::ip::address_v6 v6{addr.toArray()};
+    return Endpoint{v6, port};
+  }
 }
 
 template <class Proto>
-inline typename Proto::endpoint makeEndpoint(const IPv6Address &addr, UInt16 port)
-{
-    using Endpoint = typename Proto::endpoint;
-
-    if (!addr.valid()) {
-        return Endpoint{Proto::v6(), port};
-    } else if (addr.isV4Mapped()) {
-        asio::ip::address_v4 v4{addr.toV4().toArray()};
-        return Endpoint{v4, port};
-    } else {
-        asio::ip::address_v6 v6{addr.toArray()};
-        return Endpoint{v6, port};
-    }
+inline typename Proto::endpoint convertEndpoint(const IPv6Endpoint &endpt) {
+  return makeEndpoint<Proto>(endpt.address(), endpt.port());
 }
 
 template <class Proto>
-inline typename Proto::endpoint convertEndpoint(const IPv6Endpoint &endpt)
-{
-    return makeEndpoint<Proto>(endpt.address(), endpt.port());
-}
-
-template <class Proto>
-inline IPv6Endpoint convertEndpoint(const typename Proto::endpoint &endpt)
-{
-    return IPv6Endpoint{makeIPv6Address(endpt.address()), endpt.port()};
+inline IPv6Endpoint convertEndpoint(const typename Proto::endpoint &endpt) {
+  return IPv6Endpoint{makeIPv6Address(endpt.address()), endpt.port()};
 }
 
 }  // namespace sys
