0c9ae742c0184063286a57d88285c105d769f7bc
Bill Fisher <william.w.fisher@gmail.com>
2014-05-18 12:30:00 -0700

    ActionList is now a ProtocolList.

--- include/ofp/actionlist.h
+++ include/ofp/actionlist.h
@@ -23,35 +23,23 @@
 #define OFP_ACTIONLIST_H_
 
 #include "ofp/types.h"
-#include "ofp/bytelist.h"
+#include "ofp/protocollist.h"
 #include "ofp/actionrange.h"
 #include "ofp/actions.h"
 
 namespace ofp {
 
-class ActionList {
+class ActionList : public ProtocolList<ActionRange> {
+using Inherited = ProtocolList<ActionRange>;
 public:
-  ActionList() = default;
-
-  const UInt8 *data() const { return buf_.data(); }
-
-  size_t size() const { return buf_.size(); }
+  using Inherited::Inherited;
 
   template <class Type>
   void add(const Type &action) {
     // Make sure it's an action.
     assert(Type::type() != 0);
     buf_.add(&action, sizeof(action));
-  }
-
-  ActionRange toRange() const {
-    return ActionRange{buf_.toRange()};
-  }
-
-  void operator=(const ActionRange &range) { buf_ = range.toByteRange(); }
-
-private:
-  ByteList buf_;
+  }  
 };
 
 template <>
