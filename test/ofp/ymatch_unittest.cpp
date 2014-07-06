a60124c55f7b34a9074ce88ae8b2cdedf1fb4e7c
Bill Fisher <william.w.fisher@gmail.com>
2014-07-06 15:10:35 -0700

    Add YAML unit test for MatchBuilder.

---
+++ test/ofp/ymatch_unittest.cpp
@@ -0,0 +1,39 @@
+#include "ofp/unittest.h"
+#include "ofp/yaml/ymatch.h"
+#include "ofp/yaml/outputjson.h"
+
+using namespace ofp;
+
+
+static void diagnosticHandler(const llvm::SMDiagnostic &diag, void *context) {
+    std::cerr << diag.getMessage().str() << '\n';
+}
+
+
+TEST(ymatch, decodeMatchYaml) {
+    MatchBuilder match;
+
+    std::string input{
+R"""(---
+- field: OFB_IN_PORT
+  value: 1
+...
+)"""};
+
+    llvm::yaml::Input yin{input, nullptr, diagnosticHandler, nullptr};
+    yin >> match;
+
+    EXPECT_FALSE(yin.error());
+
+    OXMRange fields = match.toRange();
+    EXPECT_GT(fields.size(), 0);
+
+    OXMIterator iter = fields.begin();
+    EXPECT_EQ(OFB_IN_PORT::type(), iter.type());
+
+    OFB_IN_PORT inPort = iter->value<OFB_IN_PORT>();
+    EXPECT_EQ(1, inPort.value());
+
+    ++iter;
+    EXPECT_EQ(fields.end(), iter);
+}
