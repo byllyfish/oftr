b54685628948eddd1e33a4b9a70792d0d1590875
Bill Fisher <william.w.fisher@gmail.com>
2015-03-25 09:28:10 -0700

    Add 'MAX' to ControllerMaxLen enum set.

--- src/ofp/yaml/ycontrollermaxlen.cpp
+++ src/ofp/yaml/ycontrollermaxlen.cpp
@@ -12,6 +12,7 @@ using ofp::yaml::EnumConverterSparse;
 OFP_BEGIN_IGNORE_GLOBAL_CONSTRUCTOR
 
 static std::pair<OFPControllerMaxLen, llvm::StringRef> sControllerMaxLen[] = {
+    OFP_NAME(MAX),
     OFP_NAME(NO_BUFFER),
 };
 
