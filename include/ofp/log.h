ce1d134a464dfb692d11bf8a3d6d5c74f850a688
Bill Fisher <william.w.fisher@gmail.com>
2015-04-08 11:29:42 -0700

    Use std::forward in log functions.

--- include/ofp/log.h
+++ include/ofp/log.h
@@ -35,24 +35,24 @@ inline std::ostream &operator<<(std::ostream &os, const std::error_code &e) {
 }
 
 template <class Type1>
-void write_(std::ostream &os, Level level, const Type1 &value1) {
-  os << value1;
+void write_(std::ostream &os, Level level, Type1 &&value1) {
+  os << std::forward<Type1>(value1);
 }
 
 template <class Type1, class... Args>
-void write_(std::ostream &os, Level level, const Type1 &value1,
-            const Args &... args) {
-  os << value1;
+void write_(std::ostream &os, Level level, Type1 &&value1,
+            Args &&... args) {
+  os << std::forward<Type1>(value1);
   os << ' ';
-  write_(os, level, args...);
+  write_(os, level, std::forward<Args>(args)...);
 }
 
 template <class... Args>
-void write_(Level level, const Args &... args) {
+void write_(Level level, Args &&... args) {
 #if !defined(LIBOFP_LOGGING_DISABLED)
   if (level >= detail::GlobalOutputLevelFilter) {
     std::ostringstream oss;
-    write_(oss, level, args...);
+    write_(oss, level, std::forward<Args>(args)...);
     std::string buf = oss.str();
     GlobalOutputCallback(level, buf.data(), buf.size(),
                          GlobalOutputCallbackContext);
@@ -67,41 +67,41 @@ void trace(const char *type, UInt64 id, const void *data, size_t length);
 void trace_rpc(const char *type, UInt64 id, const void *data, size_t length);
 
 template <class... Args>
-inline void info(const Args &... args) {
-  detail::write_(Level::Info, args...);
+inline void info(Args &&... args) {
+  detail::write_(Level::Info, std::forward<Args>(args)...);
 }
 
 template <class... Args>
-inline void debug(const Args &... args) {
+inline void debug(Args &&... args) {
 #ifndef NDEBUG
-  detail::write_(Level::Debug, args...);
+  detail::write_(Level::Debug, std::forward<Args>(args)...);
 #endif
 }
 
 template <class... Args>
-inline void warning(const Args &... args) {
-  detail::write_(Level::Warning, args...);
+inline void warning(Args &&... args) {
+  detail::write_(Level::Warning, std::forward<Args>(args)...);
 }
 
 template <class... Args>
-inline void error(const Args &... args) {
-  detail::write_(Level::Error, args...);
+inline void error(Args &&... args) {
+  detail::write_(Level::Error, std::forward<Args>(args)...);
 }
 
 template <class... Args>
-[[noreturn]] inline void fatal(const Args &... args) {
-  detail::write_(Level::Fatal, args...);
+[[noreturn]] inline void fatal(Args &&... args) {
+  detail::write_(Level::Fatal, std::forward<Args>(args)...);
   std::abort();
 }
 
 template <class Ptr, class... Args>
-inline Ptr fatal_if_null(Ptr value, const Args &... args) {
-  return (value == nullptr) ? fatal("fatal_if_null", args...), value : value;
+inline Ptr fatal_if_null(Ptr value, Args &&... args) {
+  return (value == nullptr) ? fatal("fatal_if_null", std::forward<Args>(args)...), value : value;
 }
 
 template <class... Args>
-inline bool fatal_if_false(bool value, const Args &... args) {
-  return !value ? fatal("fatal_if_false", args...), value : value;
+inline bool fatal_if_false(bool value, Args &&... args) {
+  return !value ? fatal("fatal_if_false", std::forward<Args>(args)...), value : value;
 }
 
 // Use the LOG_LINE() macro to log source code file and line number.
