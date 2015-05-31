d200d08142b7e81bebdb5acca0b5d496dbfc7535
Bill Fisher <william.w.fisher@gmail.com>
2015-05-31 15:28:20 -0700

    Don't trace echo requests and replies.

--- src/ofp/log.cpp
+++ src/ofp/log.cpp
@@ -229,6 +229,11 @@ static void trace1(const char *type, UInt64 id, const void *data,
   }
 
   Message message{data, length};
+
+  // Do not trace echo requests or echo replies.
+  if (message.type() == OFPT_ECHO_REQUEST || message.type() == OFPT_ECHO_REPLY) 
+    return;
+
   message.transmogrify();
 
   yaml::Decoder decoder{&message};
