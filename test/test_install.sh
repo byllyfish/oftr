cbdc7aede13ac7e745e13a8f231d5427f65b3ff1
Bill Fisher <william.w.fisher@gmail.com>
2015-12-04 10:52:17 -0700

    Fix test to ignore whether there are trailing linefeeds in install_manifest.txt

--- test/test_install.sh
+++ test/test_install.sh
@@ -6,6 +6,5 @@ CURRENT_SOURCE_DIR=`dirname "$0"`
 
 # Make sure that we only install the files we expect.
 
-cd ..
-make install DESTDIR=test_install
-diff install_manifest.txt "${CURRENT_SOURCE_DIR}/install_manifest.txt"
+make -C .. install DESTDIR=test_install
+diff -b ../install_manifest.txt "${CURRENT_SOURCE_DIR}/install_manifest.txt"
