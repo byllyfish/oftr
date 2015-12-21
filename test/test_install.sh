b0b6105a23674158e52b03ef6388c1173e30f50a
Bill Fisher <william.w.fisher@gmail.com>
2015-12-21 11:44:12 -0700

    Investigate fix for launchpad test failures. [ci skip]

--- test/test_install.sh
+++ test/test_install.sh
@@ -6,5 +6,5 @@ CURRENT_SOURCE_DIR=`dirname "$0"`
 
 # Make sure that we only install the files we expect.
 
-make -C .. install DESTDIR=test_install
+make -C .. install/fast DESTDIR=test_install
 diff -b ../install_manifest.txt "${CURRENT_SOURCE_DIR}/install_manifest.txt"
