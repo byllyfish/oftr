959493793ab6fe7502db3bfe2b720497f24ac78b
Bill Fisher <william.w.fisher@gmail.com>
2016-03-05 18:19:44 -0700

    Fix gcc build failures.

--- test/ofp/instructionset_unittest.cpp
+++ test/ofp/instructionset_unittest.cpp
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-2016 William W. Fisher (at gmail dot com)
 // This file is distributed under the MIT License.
 
+#include "ofp/protocollist.h"
+
 #include "ofp/instructionlist.h"
 #include "ofp/unittest.h"
 
