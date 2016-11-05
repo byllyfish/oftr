f8503fb23adaba6968b1539b27e14bb5fabbfedd
Bill Fisher <william.w.fisher@gmail.com>
2016-11-05 14:33:16 -0700

    Annotate test output.

--- tools/libofp/test/runtests-cmdline.sh
+++ tools/libofp/test/runtests-cmdline.sh
@@ -5,12 +5,19 @@ set -e
 CURRENT_TEST_DIR=`pwd`
 LIBOFP=$CURRENT_TEST_DIR/../libofp
 
-echo "Run simple command line checks"
-
+echo "Run libofp version"
 $LIBOFP_MEMCHECK $LIBOFP version --loglevel=info
+
+echo "Run libofp jsonrpc"
 $LIBOFP_MEMCHECK $LIBOFP jsonrpc --loglevel=info < /dev/null
+
+echo "Run libofp encode"
 $LIBOFP_MEMCHECK $LIBOFP encode --loglevel=info < /dev/null
+
+echo "Run libofp decode"
 $LIBOFP_MEMCHECK $LIBOFP decode --loglevel=info < /dev/null
+
+echo "Run libofp help"
 $LIBOFP_MEMCHECK $LIBOFP help --loglevel=info
 
 exit 0
