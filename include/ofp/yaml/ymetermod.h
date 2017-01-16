d83a13e7e63ad876dfe28c9c5833d974d453478d
Bill Fisher <william.w.fisher@gmail.com>
2017-01-16 14:59:13 -0700

    Rollover copyright year to 2017.

--- include/ofp/yaml/ymetermod.h
+++ include/ofp/yaml/ymetermod.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2016 William W. Fisher (at gmail dot com)
+// Copyright (c) 2015-2017 William W. Fisher (at gmail dot com)
 // This file is distributed under the MIT License.
 
 #ifndef OFP_YAML_YMETERMOD_H_
