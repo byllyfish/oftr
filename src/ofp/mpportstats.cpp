dac80ac35921b4c10646aac83d041b22a6e3769e
Bill Fisher <william.w.fisher@gmail.com>
2017-02-20 16:34:17 -0700

    Remove unused code.

--- src/ofp/mpportstats.cpp
+++ src/ofp/mpportstats.cpp
@@ -65,26 +65,4 @@ void MPPortStatsBuilder::write(Writable *channel) {
   }
 
   channel->flush();
-
-  /**
-    if (version == OFP_VERSION_1) {
-      struct {
-        Big16 portNo;
-        Padding<6> pad;
-      } p;
-      p.portNo = UInt16_narrow_cast(msg_.portNo_);
-
-      channel->write(&p, sizeof(p));
-      channel->write(&msg_.rxPackets_, 8 * 12);
-      channel->flush();
-
-    } else if (version == OFP_VERSION_3 || version == OFP_VERSION_2) {
-      channel->write(&msg_, 104);
-      channel->flush();
-
-    } else {
-      channel->write(&msg_, sizeof(msg_));
-      channel->flush();
-    }
-    */
 }
