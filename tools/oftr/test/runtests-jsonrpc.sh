042d1119d6da8dfb638c742cda942a68547ea08e
Bill Fisher <william.w.fisher@gmail.com>
2017-11-23 10:28:19 -0700

    Fix test result.

--- tools/oftr/test/runtests-jsonrpc.sh
+++ tools/oftr/test/runtests-jsonrpc.sh
@@ -6,10 +6,13 @@ CURRENT_SOURCE_DIR=`dirname "$0"`
 CURRENT_TEST_DIR=`pwd`
 LIBOFP=$CURRENT_TEST_DIR/../oftr
 
-# Test text-based RPC protocol.
+# Test text-based RPC protocol. 
+# 
+# Use sed to strip the version/git revision information from the OFP.DESCRIPTION
+# response.
 
 echo "Test text-based RPC protocol"
-$LIBOFP jsonrpc < ${CURRENT_SOURCE_DIR}/jsonrpc-text.bin > jsonrpc-text.out
+$LIBOFP jsonrpc < ${CURRENT_SOURCE_DIR}/jsonrpc-text.bin | sed 's,"[0-9.]* ([0-9a-f]*)","",' > jsonrpc-text.out
 
 echo "Compare jsonrpc-text.out to ${CURRENT_SOURCE_DIR}/jsonrpc-text.out"
 diff jsonrpc-text.out "${CURRENT_SOURCE_DIR}/jsonrpc-text.out"
