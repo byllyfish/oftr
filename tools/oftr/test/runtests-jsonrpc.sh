39fb5091eb672afe0967af14d140f427cddd4bfe
Bill Fisher <william.w.fisher@gmail.com>
2017-11-26 10:28:42 -0700

    Rename files.

--- tools/oftr/test/runtests-jsonrpc.sh
+++ tools/oftr/test/runtests-jsonrpc.sh
@@ -7,14 +7,11 @@ CURRENT_TEST_DIR=`pwd`
 LIBOFP=$CURRENT_TEST_DIR/../oftr
 
 # Test text-based RPC protocol. 
-# 
-# Use sed to strip the version/git revision information from the OFP.DESCRIPTION
-# response.
 
 echo "Test text-based RPC protocol"
-$LIBOFP jsonrpc < ${CURRENT_SOURCE_DIR}/jsonrpc-text.bin | sed 's,"[0-9.]* ([0-9a-f]*)","",' > jsonrpc-text.out
+$LIBOFP jsonrpc < ${CURRENT_SOURCE_DIR}/jsonrpc-t.bin > jsonrpc-t.out
 
-echo "Compare jsonrpc-text.out to ${CURRENT_SOURCE_DIR}/jsonrpc-text.out"
-diff jsonrpc-text.out "${CURRENT_SOURCE_DIR}/jsonrpc-text.out"
+echo "Compare jsonrpc-t.out to ${CURRENT_SOURCE_DIR}/jsonrpc-t.out"
+diff jsonrpc-t.out "${CURRENT_SOURCE_DIR}/jsonrpc-t.out"
 
 exit 0
