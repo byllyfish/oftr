69e6c793df2b8a89566a37ffa4b5a3cbfa7b1970
Bill Fisher <william.w.fisher@gmail.com>
2017-11-28 21:47:48 -0700

    Add memcheck version of jsonrpc test.

--- tools/oftr/test/runtests-jsonrpc.sh
+++ tools/oftr/test/runtests-jsonrpc.sh
@@ -9,7 +9,7 @@ LIBOFP=$CURRENT_TEST_DIR/../oftr
 # Test text-based RPC protocol. 
 
 echo "Test text-based RPC protocol"
-$LIBOFP jsonrpc < ${CURRENT_SOURCE_DIR}/jsonrpc-t.bin > jsonrpc-t.out
+$LIBOFP_MEMCHECK $LIBOFP jsonrpc < ${CURRENT_SOURCE_DIR}/jsonrpc-t.bin > jsonrpc-t.out
 
 echo "Compare jsonrpc-t.out to ${CURRENT_SOURCE_DIR}/jsonrpc-t.out"
 diff jsonrpc-t.out "${CURRENT_SOURCE_DIR}/jsonrpc-t.out"
@@ -17,7 +17,7 @@ diff jsonrpc-t.out "${CURRENT_SOURCE_DIR}/jsonrpc-t.out"
 # Test the binary RPC protocol.
 
 echo "Test binary RPC protocol"
-$LIBOFP jsonrpc --binary-protocol < ${CURRENT_SOURCE_DIR}/jsonrpc-b.bin > jsonrpc-b.out
+$LIBOFP_MEMCHECK $LIBOFP jsonrpc --binary-protocol < ${CURRENT_SOURCE_DIR}/jsonrpc-b.bin > jsonrpc-b.out
 
 echo "Compare jsonrpc-b.out to ${CURRENT_SOURCE_DIR}/jsonrpc-b.out"
 diff jsonrpc-b.out "${CURRENT_SOURCE_DIR}/jsonrpc-b.out"
