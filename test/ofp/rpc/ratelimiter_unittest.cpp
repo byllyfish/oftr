542d5247df848bc87771f7c11554e50292c24c59
Bill Fisher <william.w.fisher@gmail.com>
2017-12-01 10:49:18 -0700

    Add test_always and test_never.

--- test/ofp/rpc/ratelimiter_unittest.cpp
+++ test/ofp/rpc/ratelimiter_unittest.cpp
@@ -46,3 +46,31 @@ TEST(ratelimiter, test_2_3) {
     std::vector<bool> expected = { true, true, false, true, true, false, true, true };
     EXPECT_EQ(actual, expected);
 }
+
+TEST(ratelimiter, test_always) {
+    RateLimiter limit{1, 1};
+    Timestamp now = Timestamp::now();
+
+    std::vector<bool> actual;
+    double events1[] = { 0.0, 2.5, 4.0, 5.0, 6.5, 9.5, 10.0, 12.6 };
+    for (auto ts: events1) {
+        actual.push_back(limit.allow(now + TimeInterval{ts}));
+    }
+
+    std::vector<bool> expected = { true, true, true, true, true, true, true, true };
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(ratelimiter, test_never) {
+    RateLimiter limit{0, 0xffffffff};
+    Timestamp now = Timestamp::now();
+
+    std::vector<bool> actual;
+    double events1[] = { 0.0, 2.5, 4.0, 5.0, 6.5, 9.5, 10.0, 12.6 };
+    for (auto ts: events1) {
+        actual.push_back(limit.allow(now + TimeInterval{ts}));
+    }
+
+    std::vector<bool> expected = { false, false, false, false, false, false, false, false };
+    EXPECT_EQ(actual, expected);
+}
