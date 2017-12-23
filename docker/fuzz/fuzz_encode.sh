5b9e26870af99d5422bfab683633bdc17f2bb919
Bill Fisher <william.w.fisher@gmail.com>
2017-12-23 11:46:59 -0700

    Fuzz testing tweaks.

--- docker/fuzz/fuzz_encode.sh
+++ docker/fuzz/fuzz_encode.sh
@@ -2,4 +2,4 @@
 #
 # Script to run afl-fuzz on `oftr encode` with yaml input.
 
-AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 afl-fuzz -i encode_in -o encode_out -- ./afl-oftr encode @@
+AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 afl-fuzz -i encode_in -o /var/log/afl/encode_out -- ./afl-oftr encode -s
