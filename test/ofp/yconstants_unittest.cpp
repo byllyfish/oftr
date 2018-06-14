3174343a8ac5f6299e46febbae5c823aec0005c8
Bill Fisher <william.w.fisher@gmail.com>
2018-06-13 17:17:25 -0700

    Fix cpplint.

--- test/ofp/yconstants_unittest.cpp
+++ test/ofp/yconstants_unittest.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2018 William W. Fisher (at gmail dot com)
+// This file is distributed under the MIT License.
+
 #include "ofp/yaml/yconstants.h"
 #include "ofp/unittest.h"
 #include "ofp/yaml/yllvm.h"
