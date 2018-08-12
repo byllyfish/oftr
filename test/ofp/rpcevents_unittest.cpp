0fde0dc4e41f032529f44a64eee5e9d04f009fc2
Bill Fisher <william.w.fisher@gmail.com>
2018-08-12 12:25:29 -0700

    Trim CHANNEL_ALERT message that ends with "\n   ^".

---
+++ test/ofp/rpcevents_unittest.cpp
@@ -0,0 +1,17 @@
+#include "ofp/unittest.h"
+#include "ofp/rpc/rpcevents.h"
+
+
+TEST(rpcevents, test_TrimErrorMessage) {
+  std::string msg1 = "x\n     ^";
+  ofp::rpc::TrimErrorMessage(msg1);
+  EXPECT_EQ(msg1, "x");
+
+  std::string msg2 = "";
+  ofp::rpc::TrimErrorMessage(msg2);
+  EXPECT_EQ(msg2, "");
+
+  std::string msg3 = "abc";
+  ofp::rpc::TrimErrorMessage(msg3);
+  EXPECT_EQ(msg3, "abc");
+}
