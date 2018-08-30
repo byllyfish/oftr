ce8a3490ab6768184279d055011e0e98518857ea
Bill Fisher <william.w.fisher@gmail.com>
2018-08-29 19:28:39 -0700

    Remove old C api.

--- include/ofp/libofp.h
+++ include/ofp/libofp.h
@@ -1,30 +1,5 @@
 // Copyright (c) 2015-2018 William W. Fisher (at gmail dot com)
 // This file is distributed under the MIT License.
-//
-// Experimental C API -- This API is a placeholder to be revisited.
-//
-// Example 1: libofp_encode
-//
-//     const char *yaml_input = "type: HELLO\nversion: 4";
-//     libofp_buffer result = { nullptr, 0 };
-//     if (libofp_encode(&result, yaml, 0) < 0) {
-//         printf("error: %s", result.data);
-//     } else {
-//         /* do something with result */
-//     }
-//     libofp_buffer_free(&result);
-//
-// Example 2: libofp_version
-//
-//     libofp_buffer result = { nullptr, 0 };
-//     libofp_version(&result);
-//     printf("version: %s", result.data);
-//     libofp_buffer_free(&result);
-//
-// The original contents of the mutable `result` buffer are ignored. The buffer
-// value is always overwritten with a newly allocated pointer. You must always
-// call `libofp_buffer_free` to free the buffer when you are done -- even when
-// there is an error.
 
 #ifndef OFP_LIBOFP_H_
 #define OFP_LIBOFP_H_
@@ -38,44 +13,10 @@ extern "C" {
 
 #define LIBOFP_EXPORT __attribute__((visibility("default")))
 #define LIBOFP_USED __attribute__((used))
-
-typedef struct {
-  void *data;
-  size_t length;
-} libofp_buffer;
-
-/// Retrieve the library version number.
-///
-/// Client is responsible for calling `libofp_buffer_free` on result buffer.
-LIBOFP_EXPORT void libofp_version(libofp_buffer *result) LIBOFP_USED;
-
-/// Translate a YAML string to a binary OpenFlow message.
-///
-/// Returns 0 if there is no error.
-/// Returns -1 and result contains error message if there is an error.
-///
-/// Client is responsible for calling `libofp_buffer_free` on result buffer.
-LIBOFP_EXPORT int libofp_encode(libofp_buffer *result, const char *yaml_input,
-                                uint32_t flags) LIBOFP_USED;
-
-/// Translate a binary OpenFlow message to YAML.
-///
-/// Returns 0 if there is no error.
-/// Returns -1 and result contains error message if there is an error.
-///
-/// Client is responsible for calling `libofp_buffer_free` on result buffer.
-LIBOFP_EXPORT int libofp_decode(libofp_buffer *result,
-                                const libofp_buffer *buffer,
-                                uint32_t flags) LIBOFP_USED;
-
-/// Dispose of `buffer` object returned by library functions.
-LIBOFP_EXPORT void libofp_buffer_free(libofp_buffer *buffer) LIBOFP_USED;
-
+	
 #define OFTR_VERSION 0
 #define OFTR_ENCODE_OPENFLOW 1
 #define OFTR_DECODE_OPENFLOW 2
-#define OFTR_ENCODE_PACKET 3
-#define OFTR_DECODE_PACKET 4
 
 /// Invoke a library function synchronously.
 ///
@@ -89,8 +30,6 @@ LIBOFP_EXPORT void libofp_buffer_free(libofp_buffer *buffer) LIBOFP_USED;
 ///       0 = get sw version  [ 0x0 | 0x0 | 0x0 | 0x0 ]
 ///       1 = encode-openflow [ OF-version | 0x0 | 0x0 | 0x1 ]
 ///       2 = decode-openflow [ 0x0 | 0x0 | 0x0 | 0x02 ]
-///       3 = encode-packet   [ 0x0 | 0x0 | 0x0 | 0x03 ]
-///       4 = decode-packet   [ 0x0 | 0x0 | 0x0 | 0x04 ]
 ///   input: Input buffer
 ///   input_len: Input buffer size
 ///   output: Mutable output buffer
