39b734d231d9cb9b7f1b183eb17b7d1035aabef4
Bill Fisher <william.w.fisher@gmail.com>
2018-12-23 09:54:54 -0700

    Undo changes to update_source script.

--- external/yaml-io/update_source.sh
+++ external/yaml-io/update_source.sh
@@ -211,11 +211,11 @@ done
 # Apply patches.
 
 patch "${WORKING_DIR}/src/Support/SourceMgr.cpp" "$WORKING_DIR/src/SourceMgr.cpp.diff"
-patch "${WORKING_DIR}/src/Support/YAMLTraits.cpp" "$WORKING_DIR/src/YAMLTraits.cpp.diff" || true
+patch "${WORKING_DIR}/src/Support/YAMLTraits.cpp" "$WORKING_DIR/src/YAMLTraits.cpp.diff"
 patch "${WORKING_DIR}/src/Support/YAMLParser.cpp" "$WORKING_DIR/src/YAMLParser.cpp.diff"
-patch "${WORKING_DIR}/include/llvm/Support/YAMLTraits.h" "$WORKING_DIR/src/YAMLTraits.h.diff" || true
+patch "${WORKING_DIR}/include/llvm/Support/YAMLTraits.h" "$WORKING_DIR/src/YAMLTraits.h.diff"
 patch "${WORKING_DIR}/unittests/Support/YAMLParserTest.cpp" "$WORKING_DIR/src/YAMLParserTest.cpp.diff"
-patch "${WORKING_DIR}/unittests/Support/YAMLIOTest.cpp" "$WORKING_DIR/src/YAMLIOTest.cpp.diff" || true
+patch "${WORKING_DIR}/unittests/Support/YAMLIOTest.cpp" "$WORKING_DIR/src/YAMLIOTest.cpp.diff"
 patch "${WORKING_DIR}/include/llvm/Support/CommandLine.h" "$WORKING_DIR/src/CommandLine.h.diff"
 patch "${WORKING_DIR}/src/Support/CommandLine.cpp" "$WORKING_DIR/src/CommandLine.cpp.diff"
 
