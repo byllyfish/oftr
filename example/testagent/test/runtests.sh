9d4f0ee09fb9c73d12263f928d2ea5d06148fcda
Bill Fisher <william.w.fisher@gmail.com>
2019-04-14 17:32:38 -0700

    Make sure multiple tests can run in parallel.

--- example/testagent/test/runtests.sh
+++ example/testagent/test/runtests.sh
@@ -15,11 +15,12 @@ echo "Working Directory:" `pwd`
 for input in $CURRENT_SOURCE_DIR/*.in ; do
   name=`basename $input .in`
   output=$name.out
+  output_tmp="$name-${LIBOFP_DEFAULT_PORT}.out"
   echo "Run testagent to convert $input into $output"
   # Remove lines with timestamps from output.
-  $TESTAGENT "127.0.0.1:$LIBOFP_DEFAULT_PORT" < $input | grep -Ev "^time: +[1-9][0-9]*\.[0-9]+$" > $output
+  $TESTAGENT "127.0.0.1:$LIBOFP_DEFAULT_PORT" < $input | grep -Ev "^time: +[1-9][0-9]*\.[0-9]+$" > $output_tmp
   echo "Compare $output and $CURRENT_SOURCE_DIR/$output"
-  diff $output $CURRENT_SOURCE_DIR/$output 
+  diff $output_tmp $CURRENT_SOURCE_DIR/$output 
 done
 
 exit 0
